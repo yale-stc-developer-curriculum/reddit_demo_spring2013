b952cf9edb386a6bd1a8314900623913f4e3d7f0
Adam Bray <adam.bray@yale.edu>
2013-03-27 18:29:10 -0400

    changes from week 3 class: added controller actions for managing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(params[:user])
+    redirect_to users_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:user])
+    redirect_to @user
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 RedditDemo::Application.routes.draw do
-  match 'users/' => 'users#index'
-  match 'users/:id' => 'users#show'
+  resources :users
   root to: 'users#index'
 end
